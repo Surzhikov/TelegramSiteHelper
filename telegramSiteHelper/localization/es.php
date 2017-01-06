da52f9e51064019655b9dfa4862bff08e1a36b30
Alexander Rodriguez <alexr1712@gmail.com>
2017-01-06 19:45:50 -0400

    Add Spanish Locale

---
+++ telegramSiteHelper/localization/es.php
@@ -0,0 +1,21 @@
+<?php
+$_localization["serverStoppedByCommand"]="Se detuvo el servidor por el comando stopserver";
+$_localization["authOk"]="Gracias! has ingresado con exito! Ahora puedes recibir todos los mensajes de los usuarios en este chat.";
+$_localization["pleaseEnterManagerPassword"]="Ingresa la clave de Asesor o Gestor";
+$_localization["goingOffline"]="Modo Fuera de Linea. No recibiras nuevos mensajes!, Para estar de nuevo en linea - enviame el comando /online";
+$_localization["goingOnline"]="De nuevo recibiras los mensajes de los usuarios!";
+$_localization["youQuit"]="Sesion Cerrada. Para ingresar ingrese la clave de acceso.";
+$_localization["allMessagesAreGoingToChat"]="Ahora, tus mensajes se enviaran a ese chat";
+$_localization["chat"]="Chat";
+$_localization["notAvailible"]="No Disponible";
+$_localization["manager"]="Asesor";
+$_localization["client"]="Cliente";
+$_localization["fullHistory"]="Historial de Chat";
+$_localization["viewHistory"]="Ver Historial";
+$_localization["goToOtherChat"]="Ingresar a este chat, envia";
+$_localization["forAnswerGoToChat"]="Para ingresar a este chat, escribe";
+$_localization["otherManagersChat"]="Este chat a sido tomado por otro asesor!";
+$_localization["pleaseSelectChatFirst"]="Para enviar un mensaje primero debes seleccionar el chat!";
+$_localization["serverRestart"]="El servidor sera reiniciado en un minuto";
+$_localization["serverAlreadyRunning"]="¡Error! El servidor ya está funcionando. ¡Accion Detenida!";
+?>
